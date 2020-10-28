b757563f3311a3f4ab270beaefe71051e825eae2
Nate Chiles <nate.chiles@gmail.com>
2020-10-27 21:29:39 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 5000;
+
+const { Pool } = require('pg');
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false
+	}
+});
+
+app.use(express.static(path.join(__dirname, '/')))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('views', path.join(__dirname, '/'))
+app.set('view engine', 'ejs')
+
+
+app.get('/', async (req, res) => {
+	try {
+		const client = await pool.connect();
+		const result = await client.query('SELECT * FROM winners_table');
+		const results = { 'results': (result) ? result.rows : null};
+		console.log(results);
+		res.render('index', results );
+		client.release();
+	} catch (err) {
+		console.error(err);
+		res.send("Error " + err);
+	}
+})
+
+app.post('/', (req, res) => {
+	const text = 'INSERT INTO winners_table(name, prize) VALUES($1, $2)'
+	const values = [req.body.name, req.body.prize];
+
+	pool.query(text, values, (error, results) => {
+		if (error) {
+			throw error
+		}
+		res.redirect('back');
+	})
+})
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
